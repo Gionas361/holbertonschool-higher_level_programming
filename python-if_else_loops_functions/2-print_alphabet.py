3f7837e7a74ef1b38cfc85b19d46c133cf9c4e2f
Gionas361 <giovannicarmona57@gmail.com>
2023-09-26 10:04:14 -0400

    test

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    print(chr(letter), end="")
+letter = 97
+while letter < 123:
+    print("{}".format(chr(letter)), end="")
+    letter = letter + 1
