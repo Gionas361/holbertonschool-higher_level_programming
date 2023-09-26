a9ccf4d04c07b5490c8063e25c842667b06ec5a3
Gionas361 <giovannicarmona57@gmail.com>
2023-09-26 13:27:09 -0400

    fizzbuzz done

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 letter = 97
 while letter < 123:
-    if letter == 113 or letter == 101:
-        letter = letter + 1
-    else:
-        print("{}".format(chr(letter)), end="")
-        letter = letter + 1
+	if letter == 113 or letter == 101:
+		letter = letter + 1
+	else:
+		print("{}".format(chr(letter)), end="")
+		letter = letter + 1
