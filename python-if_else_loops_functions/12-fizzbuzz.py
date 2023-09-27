2db8d3b7c2efc3ad7eae1c1af16ebc6b0d726b12
Gionas361 <giovannicarmona57@gmail.com>
2023-09-27 10:11:31 -0400

    pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	for num in range(1, 101):
-		if ((num % 3) == 0):
-			print("Fizz", end="")
-		if ((num % 5) == 0):
-			print("Buzz", end="")
-		if ((num % 3) != 0) and ((num % 5) != 0):
-			print("{}".format(num), end="")
-		if (num != 100):
-			print(" ", end="")
-		num = num + 1
+    for num in range(1, 101):
+        if ((num % 3) == 0):
+            print("Fizz", end="")
+        if ((num % 5) == 0):
+            print("Buzz", end="")
+        if ((num % 3) != 0) and ((num % 5) != 0):
+            print("{}".format(num), end="")
+        if (num != 100):
+            print(" ", end="")
+        num = num + 1
