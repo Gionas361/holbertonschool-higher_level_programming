931800252133d8f91b7d81e01ea7b07e3872a165
Gionas361 <giovannicarmona57@gmail.com>
2023-09-27 14:01:31 -0400

    add args

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    arg = 0
+    currarg = 0
+    count = len(sys.argv) - 1
+
+    for i in range(count):
+        arg = arg + 1
+        result = int(result) + int(sys.argv[arg])
+    print("{}".format(result))
