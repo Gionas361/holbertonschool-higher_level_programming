891000839cd64e1ac58ae05feeca6e87bd894ae1
Gionas361 <giovannicarmona57@gmail.com>
2023-09-28 09:26:00 -0400

    pycodestyle

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for c in str:
-		if c >= chr(97) and c <= chr(122):
-			c = chr(ord(c) - 32)
-		print("{}".format(c), end="")
-	print("")
+    for c in str:
+        if c >= chr(97) and c <= chr(122):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	if number is int:
-		number = str(number)
-	number = int(str(number)[-1:])
-	print(number, end="")
-	return(number)
+    if number is int:
+        number = str(number)
+    number = int(str(number)[-1:])
+    print(number, end="")
+    return(number)
