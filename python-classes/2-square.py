09e2b02786fe753385688625d55e29307e1d52af
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-10 10:13:05 -0400

    error?

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class named SQUARE"""
+
+
+class Square:
+    """S Q U A R E."""
+
+    def __init__(self, size=0):
+        """Start new square"""
+        if size is int:
+            TypeError("size must be an integer")
+        elif size < 0:
+            ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
