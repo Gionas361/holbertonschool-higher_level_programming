74d25273022320816c6fab7bda009f887a67509d
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-10 10:15:52 -0400

    isinstance

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """Start new square"""
-        if size is int:
+        if not isinstance(size, int):
             TypeError("size must be an integer")
         elif size < 0:
             ValueError("size must be >= 0")
