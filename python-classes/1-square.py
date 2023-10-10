9a5a77574e6e21b497367c7aebefa0077c17b6d5
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-10 10:03:42 -0400

    squearaer

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class named SQUARE"""
+
+
+class Square:
+    """S Q U A R E."""
+
+    def __init__(self, size):
+        """Start new square"""
+        self.__size = size
