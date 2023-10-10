75eb52bff0e38fdee526beafcc3c3f41e717c505
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-10 13:16:00 -0400

    test 1

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Define a class named SQUARE"""
+
+
+class Square:
+    """S Q U A R E."""
+
+    def __init__(self, size=0):
+        """Start new square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Determined sizw?"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Squared size?"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """A R E A"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in self.__size:
+                while range(0, self.__size):
+                    print("#", end="")
+                print("")
