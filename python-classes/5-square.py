f4bdb9cbbd5a56a15c0dc44d4a59c5953657836b
Gionas361 <giovannicarmona57@gmail.com>
2023-10-11 22:16:06 -0400

    ending

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,10 +28,9 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
+        """SQUARE PRINTER"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
             print("")
-        else:
-            for i in self.__size:
-                while range(0, self.__size):
-                    print("#", end="")
-                print("")
