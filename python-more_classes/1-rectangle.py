c434a6cf1513ef168dc6fafcceb0eb5c22dd793c
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-13 10:55:13 -0400

    reverse logic

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,9 +17,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if not isinstance(self.__width, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if not self.__width >= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,6 +32,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if not value >= 0:
             raise ValueError("height must be >= 0")
         self.__height = value
