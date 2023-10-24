1f088fa2d3ce820dca63387f12ac9408fafb6f35
“Gionas361” <giovannicarmona57@gmail.com>
2023-10-24 09:31:43 -0400

    zip

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -11,6 +11,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for k, v in json.items():
+            setattr(self, k, v)
