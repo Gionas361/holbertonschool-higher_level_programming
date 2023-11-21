fc3c52a00759265c82bf79b39835a18649c3ca98
Gionas361 <giovannicarmona57@gmail.com>
2023-11-21 19:27:23 -0400

    declaring i

--- EXTRA-TASKS/Coin_Flip.py
+++ EXTRA-TASKS/Coin_Flip.py
@@ -4,12 +4,14 @@ import sys
 
 aguments = sys.argv
 print("Got the arguments: ", len(aguments))
+i = 0
 while i <= len(aguments):
     print(aguments[i], i)
     i += 1
 
 def Flips(amount):
     Head_or_Tails = 0
+    i = 0
 
     while i <= amount:
         Head_or_Tails = random.randint(0,1)
