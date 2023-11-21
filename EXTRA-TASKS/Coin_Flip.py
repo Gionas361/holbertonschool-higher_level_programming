f8554c93d4dc63b0a016e2df3550f1341d664949
Gionas361 <giovannicarmona57@gmail.com>
2023-11-21 19:32:54 -0400

    correct argument

--- EXTRA-TASKS/Coin_Flip.py
+++ EXTRA-TASKS/Coin_Flip.py
@@ -30,7 +30,7 @@ def Argument_Check(aguments):
     elif len(aguments) == 1:
         return (print("Please provide a number of times to flip the coin."))
     elif len(aguments) == 2:
-        Flips(aguments[2])
+        Flips(aguments[1])
 
 
 # //////////////////////////// Tests ///////////////////////////
