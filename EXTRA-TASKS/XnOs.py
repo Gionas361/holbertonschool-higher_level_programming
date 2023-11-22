e36ca36b999f4a16d11c068d68b55696d28e1f6c
Gionas361 <giovannicarmona57@gmail.com>
2023-11-22 09:04:42 -0400

    test emojis

---
+++ EXTRA-TASKS/XnOs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+import sys
+
+
+# Variables containin the emojis the guide to reading the results
+# Results
+Win = "\U0001F31D" # Not gonna be used lmao
+Loss = "\U0001F31A"
+Tie = "\U0001F501"
+
+# Moves for players
+Boxes = "\U0001F532"
+X = "\U0001F4A2"
+O = "\U0001F518"
+
+print(f'{Boxes} {X} {O}')
